14e5772a86f6258c1cf7cb8321788b404810637a
0408petrus <petrus.marcellino15@students.usu.ac.id>
2025-02-27 10:56:21 +0700

    Feat:Fix Image Issue in DrugList.tsx

--- easy-pharmacy-app/src/components/DrugItem.tsx
+++ easy-pharmacy-app/src/components/DrugItem.tsx
@@ -24,20 +24,21 @@ export default function DrugItem({ drug }: { drug: DrugType }) {
   }
 
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden transform transition duration-500 hover:scale-105">
-      <div className="w-full h-48 flex items-center justify-center bg-gray-200">
-        <img src={drug.imageURL} alt="" className="max-h-full max-w-full object-contain" />
-      </div>
+    <>
+    <div className="rounded-lg overflow-hidden transform transition duration-500 hover:scale-105">
+      <img src={drug.imageURL} alt="" className="w-full h-48 object-cover" />
       <div className="p-4 flex flex-col justify-between items-baseline">
-      <h3 className="font-bold text-dark text-xl mb-2">{drug.title}</h3>
-      <div className="text-dark-400 mb-4">{drug.description}</div>
-      <div className="text-dark-400 mb-4">Price: {drug.price}</div>
-      <div className="text-dark-400 mb-4">Stock: {drug.stock}</div>
-      <div className="grid grid-cols-2 gap-x-5 mt-auto w-full">
-        <Button onClick={handleReadMore} label="Read More" className="hover:bg-black"/>
-        <Button onClick={handleChart} label="Add to Cart" className="bg-orange-500 hover:bg-green-600" disabled={drug.stock === 0} />
-      </div>
+        <h3 className="font-bold text-dark text-xl mb-2">{drug.title}</h3>
+        <div className="text-dark-400 mb-4">{drug.description}</div>
+        <div className="text-dark-400 mb-4">Price: {drug.price}</div>
+        <div className="text-dark-400 mb-4">Stock: {drug.stock}</div>
+        <div className="grid grid-cols-2 gap-x-5 mt-auto w-full">
+          <Button onClick={handleReadMore} label="Read More" className="hover:bg-black"/>
+          <Button onClick={handleChart} label="Add to Cart" className="bg-orange-500 hover:bg-green-600" disabled={drug.stock === 0} />
+        </div>
       </div>
     </div>
+      
+    </>
   )
 }
